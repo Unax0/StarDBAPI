2e264a5fb18345fa304e396a52ede995f316dc08
Vyacheslav <34347@iitu.edu.kz>
2025-03-04 15:48:23 +0500

    add error indicator component and integrate error handling in random planet

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Spinner from '../spinner'
 import SwapiServise from '../../services/swapi-service'
+import ErrorIndicator from "../error-indicator";
 
 import './random-planet.css'
 
@@ -9,12 +10,20 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     };
 
     constructor(props) {
         super(props);
         this.updatePlanet();
+    }
+
+    onError = (err) => {
+        this.setState({
+            loading: false,
+            error: true
+        })
     }
 
     onPlanetLoaded = (planet) => {
@@ -29,15 +38,20 @@ export default class RandomPlanet extends Component {
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch((err) => this.onError())
     }
 
     render() {
-        const {planet, loading} = this.state;
+        const {planet, loading, error} = this.state
+
+        const hasData = !(loading || error)
+
         const spinner = loading ? <Spinner />: null
-        const content = !loading ? <PlanetView planet={planet}/>: null
+        const content = hasData ? <PlanetView planet={planet} /> : null
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
